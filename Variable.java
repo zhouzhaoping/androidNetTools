538659be6602aac4921b2f90e155d523e06dd718
Zhou Zhaoping <zhouzhaoping11@gmail.com>
2014-05-22 08:39:47 +0800

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,10 @@
+package william.learnitbyself.sampleapp;
+
+// 数据库字段
+public final class Variable
+{
+    static String activity[] = {"title", "publish_time", "content"};
+	static String success[] = {"Success"};
+	static String comment[] = {"activity_ID", "publisher_ID", "publish_time", "content"};
+	
+}
