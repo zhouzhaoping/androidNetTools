12d52e8f4d3f6278326d8c9ccaa6a5c542fe00db
Zhou Zhaoping <zhouzhaoping11@gmail.com>
2014-05-23 13:43:10 +0800

    Update Variable.java

--- Variable.java
+++ Variable.java
@@ -3,7 +3,7 @@ package william.learnitbyself.sampleapp;
 // 数据库字段
 public final class Variable
 {
-	
+	static String url = "";
 	static String activity[] = {"title", "publish_time", "content"};
 	static String success[] = {"success"};
 	static String comment[] = {"activity_ID", "publisher_ID", "publish_time", "content"};
